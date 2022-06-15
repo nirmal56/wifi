e04158a73f62c829c32a7163dc8f7f198895aef2
nirmal56 <nirmal.patel@einfochips.com>
2022-06-15 13:12:23 +0530

    chkstatus working

---
+++ day3/checkenum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <glib.h>
+#include <NetworkManager.h>
+
+#define NM_CONNECTIVITY_UNKNOWN
+#define NM_CONNECTIVITY_NONE
+#define NM_CONNECTIVITY_FULL
+
+int main(){
+    
+}
