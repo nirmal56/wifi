2555e5eebbd696a11128d4fd50aeab8fb7af4a20
nirmal56 <nirmal.patel@einfochips.com>
2022-06-22 18:13:53 +0530

    trial for multiple placeholder

--- day5/callbackSir.cpp
+++ day5/callbackSir.cpp
@@ -7,6 +7,7 @@ class RedundencyManager // incl. Typo ;-)
 
     using FsEventCallabck = std::function<std::string(int data)>;
     using callback2 = std::function<std::string(int tada, int taadaa)>;
+    using callback3 = std::function<void(std::string a,std::string b,std::string c,std::string d,std::string e)>;
 
 public:
     // std::function<return_type(list of argument_type(s))>
@@ -18,6 +19,10 @@ public:
     {
         return f(data, tada);
     }
+    void Init(callback3 f,std::string a,std::string b,std::string c,std::string d,std::string e)
+    {
+        return f(a,b,c,d,e);
+    }
 };
 
 class CLoggersInfra
@@ -26,6 +31,11 @@ private:
     std::string member = "Hello from non static member callback!";
 
 public:
+    void fiveargfun(std::string a,std::string b,std::string c,std::string d,std::string e){
+        std::cout << a << " " << b << " " << c << " " << d << " " << e << "\n"; 
+        // return a + " " + b + " " + c + " " + d + " " + e ; 
+    }
+
     static std::string RedundencyManagerCallBack(int data)
     {
         return "Hello from static member callback! " + std::to_string(data);
@@ -63,6 +73,14 @@ int main()
     auto callback = std::bind(&CLoggersInfra::NonStaticRedundencyManagerCallBack,CLoggersInfra(),std::placeholders::_1);
     std::cout<<instance.Init(callback,5)<<"\n";
 
-    auto callback3 = std::bind(&CLoggersInfra::twoargfun, CLoggersInfra(), std::placeholders::_1, std::placeholders::_2);
-    std::cout << instance.Init(callback3, 1, 2) << "\n";
+    // auto callback3 = std::bind(&CLoggersInfra::twoargfun, CLoggersInfra(), std::placeholders::_2, std::placeholders::_1);
+    // std::cout << instance.Init(callback3, 1, 2) << "\n";
+
+    auto callback5 =std::bind(&CLoggersInfra::fiveargfun,CLoggersInfra(),
+                                                        std::placeholders::_1,
+                                                        std::placeholders::_2,
+                                                        std::placeholders::_3,
+                                                        std::placeholders::_4,
+                                                        std::placeholders::_5);
+    instance.Init(callback5,"this","is","data","to","printed");
 }
